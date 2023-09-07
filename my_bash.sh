fc8520fd496ba236a1c54640469c12367b96d76e
Kjetil Fjellheim <kjetil@forgottendonkey.net>
2023-09-07 23:01:58 +0200

    Added configuration for completion and git config
    Notify are for kde notification in scripts.
    Git config are now automatically set in the script so that they can be used on multiple computers.
    Started completion for clone command.

---
+++ my_bash.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+LIGHTGRAY="\033[0;37m"
+WHITE="\033[1;37m"
+BLACK="\033[0;30m"
+DARKGRAY="\033[1;30m"
+RED="\033[0;31m"
+LIGHTRED="\033[1;31m"
+GREEN="\033[0;32m"
+LIGHTGREEN="\033[1;32m"
+BROWN="\033[0;33m"
+YELLOW="\033[1;33m"
+BLUE="\033[0;34m"
+LIGHTBLUE="\033[1;34m"
+MAGENTA="\033[0;35m"
+LIGHTMAGENTA="\033[1;35m"
+CYAN="\033[0;36m"
+LIGHTCYAN="\033[1;36m"
+NOCOLOR="\033[0m"
+
+export PS1="\n$WHITE\u@\h $RED\w$GREEN\$(GIT_PS1_SHOWUNTRACKEDFILES=1 GIT_PS1_SHOWDIRTYSTATE=1 __git_ps1)$NOCOLOR$ "
+
+export CLICOLOR=1
+export LS_COLORS='no=00:fi=00:di=00;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:*.xml=00;31:'
+
+clone() {
+    echo Cloning git@github.com\:kjetilfjellheim/${1}.git to ~/Code/$1
+    cd ~/Code
+    git clone git@github.com:kjetilfjellheim/${1}.git ${1}
+    cd $1
+    echo "Completed cloning"
+}
+
+notify() {
+    kdialog --passivepopup "${1}" 5
+}
+
+complete -W "my_bash rust_taxonomy rust_iawk rust_game rust_fractals" clone
+
+git config --global color.ui true
+
+git config --global color.diff-highlight.oldNormal    "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal    "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+
+git config --global color.diff.meta       "11"
+git config --global color.diff.frag       "magenta bold"
+git config --global color.diff.func       "146 bold"
+git config --global color.diff.commit     "yellow bold"
+git config --global color.diff.old        "red bold"
+git config --global color.diff.new        "green bold"
+git config --global color.diff.whitespace "red reverse"
+
+git config --global core.editor "code --wait --new-window"
+git config --global diff.tool vscode
+git config --global difftool.vscode.cmd 'code --wait --diff $LOCAL $REMOTE'
+git config --global merge.tool vscode
+git config --global mergetool.vscode.cmd 'code --wait $MERGED'
+
+git config --global user.name "Kjetil Fjellheim"
+git config --global user.email kjetil@forgottendonkey.net
+git config --global commit.template ~/Code/my_bash/gittemplate.txt
