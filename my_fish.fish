3fd1cfe86c4b446ad0fcef95a5c85b544b7d6a27
Kjetil Fjellheim <kjetil@forgottendonkey.net>
2023-09-09 22:01:39 +0200

    feat: Remove unused code
    Removed a lot of unused code and fixed completion for clone command.
    Now it stores the clone completion in the ~/.config/fish/completions/
    so that it's no longer necessary to  run the completion every time.
    
    Breaking chnages: None
    
    Resolves: None

--- my_fish.fish
+++ my_fish.fish
@@ -5,28 +5,20 @@ set -x MY_GIT_NAME "Kjetil Fjellheim"
 set -x MY_GIT_EMAIL "kjetil@forgottendonkey.net"
 set -x MY_GIT_PATH "/home/kjetil/Code"
 
-#Common color information
-set LIGHTGRAY "\033[0;37m"
-set WHITE "\033[1;37m"
-set BLACK "\033[0;30m"
-set DARKGRAY "\033[1;30m"
-set RED "\033[0;31m"
-set LIGHTRED "\033[1;31m"
-set GREEN "\033[0;32m"
-set LIGHTGREEN "\033[1;32m" 
-set BROWN "\033[0;33m"
-set YELLOW "\033[1;33m"
-set BLUE "\033[0;34m"
-set LIGHTBLUE "\033[1;34m"
-set MAGENTA "\033[0;35m"
-set LIGHTMAGENTA "\033[1;35m"
-set CYAN "\033[0;36m"
-set LIGHTCYAN "\033[1;36m"
-set NOCOLOR "\033[0m"
-
 # Set the prompt
-set -x PS1 "\n$WHITE\u@\h $RED\w$GREEN\$(GIT_PS1_SHOWUNTRACKEDFILES=1 GIT_PS1_SHOWDIRTYSTATE=1 __git_ps1)$NOCOLOR"
-set -x LS_COLORS 'no=00:fi=00:di=00;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:*.xml=00;31:'
+function fish_prompt
+    set_color white
+    set -l git_branch (git branch 2>/dev/null | sed -n '/\* /s///p')
+    echo -n (whoami)'@'(hostname)':'
+    set_color $fish_color_cwd
+    echo -n (prompt_pwd --full-length-dirs=2 --dir-length=2) 
+    set_color purple
+    echo -n '{'
+    echo -n "$git_branch"
+    echo -n '}'
+    set_color normal
+    echo -n ' $ '
+end
 
 #Use git color ui
 git config --global color.ui true
@@ -85,6 +77,7 @@ function update_repos_completion
    end   
    set repos (curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $token" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/users/$MY_GIT_USERNAME/repos?per_page=100" | jq '.[] | .ssh_url' | cut -d'/' -f2 | sed 's/\.git\"//g' | tr '\n' ' ')
    echo "$repos"
-   complete -a "$repos" clone
+   echo "complete -r -f -c \"clone\" -a \"$repos\" " > ~/.config/fish/completions/clone.fish
 end
 
+bind \t complete
